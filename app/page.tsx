578360ee736869aace1c35f6dddcc1eba36553b8
giancarlosgiron-art <giancarlosgiron@gmail.com>
2026-05-15 00:28:01 -0400

    Fix: force-dynamic en home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { fetchProducts, fetchTopProducts } from "@/lib/api";
 import { Product } from "@/lib/types";
 import Hero from "@/components/home/Hero";
